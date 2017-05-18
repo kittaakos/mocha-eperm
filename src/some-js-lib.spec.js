6c94d086250971b079b3a43ec3a1e80a3c059868
Akos Kitta <kittaakos@gmail.com>
2017-05-18 14:44:37 +0200

    Added `beforeEach`.
    Signed-off-by: Akos Kitta <kittaakos@gmail.com>

--- src/some-js-lib.spec.js
+++ src/some-js-lib.spec.js
@@ -1,8 +1,14 @@
 const fs = require("fs-extra");
 const expect = require("chai").expect;
 const { mkdir, tmpDirPath } = require("./some-js-lib");
+const uuidV1 = require('uuid/v1');
 
 describe("some-js-lib", function() {
+
+    beforeEach(function() {
+        fs.mkdirSync(tmpDirPath("before-all-" + uuidV1()))
+    });
+
     it("Should create a directory under the temp folder.", function() {
         const dirPath = tmpDirPath("foo-js");
         mkdir(dirPath);
--- src/some-ts-lib.spec.ts
+++ src/some-ts-lib.spec.ts
@@ -3,9 +3,15 @@ import * as chai from "chai";
 import * as fs from "fs-extra";
 import { mkdir, tmpDirPath } from "./some-ts-lib";
 
+const uuidV1 = require('uuid/v1');
 const expect = chai.expect;
 
 describe("some-ts-lib", () => {
+
+    beforeEach(() => {
+        fs.mkdirSync(tmpDirPath("before-all-" + uuidV1()));
+    });
+
     it("Should create a directory under the temp folder.", () => {
         const dirPath = tmpDirPath("foo-ts");
         mkdir(dirPath);
